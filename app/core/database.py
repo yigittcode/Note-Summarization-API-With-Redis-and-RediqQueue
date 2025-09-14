947c954b8e4181f7b191cb65cb9f072a8bbef61f
yigittcode <yigitabdullah329@gmail.com>
2025-09-14 17:49:20 +0300

    aysnc database file is created with sqalchemy orm

---
+++ app/core/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import declarative_base
+from app.core.config import settings
+
+engine = create_async_engine(settings.database_url, echo=True)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+Base = declarative_base()
+
+
+async def get_db() -> AsyncSession:
+    async with async_session_maker() as session:
+        yield session
