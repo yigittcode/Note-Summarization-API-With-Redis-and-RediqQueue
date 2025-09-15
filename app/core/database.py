d9369903ac1290558d63394696f0180b00da71c8
yigittcode <yigitabdullah329@gmail.com>
2025-09-15 16:37:26 +0300

    Enable pool_pre_ping to prevent connection timeouts

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from sqlalchemy.orm import declarative_base
 from app.core.config import settings
 
-engine = create_async_engine(settings.database_url, echo=True)
+engine = create_async_engine(settings.database_url, echo=True , pool_size=10, max_overflow=20 , pool_pre_ping=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 Base = declarative_base()
